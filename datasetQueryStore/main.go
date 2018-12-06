fb2d6ecba6a1df1f15d5f6e0a4a80fdc7c3c7970
madisonsmartin <madison.53.martin@gmail.com>
2018-12-05 20:54:16 -0600

    .

--- datasetQueryStore/main.go
+++ datasetQueryStore/main.go
@@ -70,7 +70,7 @@ func (s store) GetDataset(ctx context.Context, data *dspb.Dataset) (*dspb.Datase
 	return &dspb.Dataset{}, nil
 }
 func (s store) GetDatasets(ctx context.Context, data *dspb.Dataset) (*dspb.MultipleDatasets, error) {
-	log.Print("query store: query Dataset request")
+	/*log.Print("query store: query Dataset request")
 
 	db, err := sql.Open("postgres", connectionstring)
 	defer db.Close()
@@ -110,7 +110,8 @@ func (s store) GetDatasets(ctx context.Context, data *dspb.Dataset) (*dspb.Multi
 		log.Fatal(err)
 	}
 
-	return datasets, nil
+	return datasets, nil*/
+	return nil, nil
 }
 func (s store) CreateDataset(ctx context.Context, data *dspb.Dataset) (*dspb.Dataset, error) {
 	log.Print("query store: create Dataset request")
